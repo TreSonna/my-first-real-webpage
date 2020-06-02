963f10aab466cf14048b45b4b2fc919a8fc2e9ce
TreSonna <59778418+TreSonna@users.noreply.github.com>
2020-06-02 11:40:00 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("button#white").click(function() {
+      $("body").removeClass();
+      $("body").addClass("white");
+    });
+  });
+
+  $(document).ready(function() {
+    $("button#black").click(function() {
+      $("body").removeClass();
+      $("body").addClass("black");
+    });
+  });
